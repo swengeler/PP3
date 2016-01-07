d7ca1531ddc09af85e59d75fea4f55c4f3c1d8cd
Nicola Gheza <me@nicolagheza.com>
2016-01-07 12:26:37 +0100

    Added fillGaps() in CargoSpace.java

--- Greedy Algorithm/GreedyAlgorithm.java
+++ Greedy Algorithm/GreedyAlgorithm.java
@@ -5,44 +5,44 @@ import javax.swing.JPanel;
 import java.awt.BorderLayout;
 
 /**
-* A class that uses a greedy algorithm to determine a possible (most likely not optimal) 
+* A class that uses a greedy algorithm to determine a possible (most likely not optimal)
 * packing solution for a certain number of type A, B and C packages that are supposed
 * to fit into a cargo area of 16.5m x 2.5m x 4m.
 *
-* @author Simon Wengeler 
+* @author Simon Wengeler
 */
 public class GreedyAlgorithm {
-    
+
     /**
     * The number of times the algorithm is used to calculate a solution in order to find the best one
     * out of those.
     */
     private static final int NR_RUNS = 1;
-    
+
     /**
     * A list (in array form) of the packages which are supposed to be placed in the cargo space.
     */
     private static Package[] packages;
-    
+
     /**
     * The main method of the class, initialising the array used for the internal representation
-    * of the cargo space, asking for the number of certain package types to be used and 
+    * of the cargo space, asking for the number of certain package types to be used and
     * determining a packing for them.
-    * 
+    *
     * @param args Not used.
     */
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        
+
         CargoSpace[] allCS = new CargoSpace[NR_RUNS];
-        
+
         System.out.print("Please enter the number of A packages: ");
         int nrA = in.nextInt();
         System.out.print("Please enter the number of B packages: ");
         int nrB = in.nextInt();
         System.out.print("Please enter the number of C packages: ");
         int nrC = in.nextInt();
-        
+
         int nrPackages = nrA + nrB + nrC;
         packages = new Package[nrPackages];
         for (int i = 0; i < nrC; i++)
@@ -51,15 +51,17 @@ public class GreedyAlgorithm {
             packages[i] = new Package(PackageType.BPackage);
         for (int i = nrC + nrB; i < nrPackages; i++)
             packages[i] = new Package(PackageType.APackage);
-        
+
         ArrayList<Integer> randomNumbers;
-        
+        ArrayList<Package> packagesLeft;
+
         for (int i = 0; i < NR_RUNS; i++) {
             CargoSpace cs = new CargoSpace(33, 5, 8);
             boolean done = false;
             int counter = 0;
             randomNumbers = new ArrayList<Integer>();
-            for (int j = 0; j < nrPackages; j++) 
+            packagesLeft = new ArrayList<Package>();
+            for (int j = 0; j < nrPackages; j++)
                 randomNumbers.add(new Integer(j));
             //Collections.shuffle(randomNumbers);
             while (!done) {
@@ -69,15 +71,18 @@ public class GreedyAlgorithm {
                 if (!cs.overlap(p))
                     cs.putPackage(p);
                 else
-                    done = true;
-                counter++;   
+                    packagesLeft.add(p);
 
-                if (counter >= nrPackages) 
+                counter++;
+
+                if (counter >= nrPackages)
                     done = true;
             }
+            System.out.println("There are " + packagesLeft.size() + " packages left");
+            cs.fillGaps(packagesLeft);
             allCS[i] = cs;
         }
-        
+
         double best = 0;
         CargoSpace bestCS = new CargoSpace(0, 0, 0);
         for (int i = 0; i < allCS.length; i++) {
@@ -88,15 +93,15 @@ public class GreedyAlgorithm {
             }
         }
         System.out.println("BEST VALUE: " + bestCS.getTotalValue());
-        
+
         JFrame f = new JFrame();
         f.setSize(1000, 1020);
-        
+
         Display display = new Display(bestCS.getArray());
         f.add(display, BorderLayout.CENTER);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setVisible(true);
-        
+
     }
-    
+
 }
